b3fb9a24f4c22aa2649ea4a475ac75d394fd2342
sonu <skr3704@gmail.com>
2022-01-13 13:44:34 +0530

    implicit deep linking completed

--- app/src/main/java/com/sonu/jetpacknavigationexample/LoginFragment.kt
+++ app/src/main/java/com/sonu/jetpacknavigationexample/LoginFragment.kt
@@ -1,14 +1,21 @@
 package com.sonu.jetpacknavigationexample
 
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.sonu.jetpacknavigationexample.databinding.FragmentLoginBinding
 
 class LoginFragment : BaseFragment<FragmentLoginBinding>() {
+
+    private val args: LoginFragmentArgs by navArgs()
 
     override fun getViewBinding(): FragmentLoginBinding =
         FragmentLoginBinding.inflate(layoutInflater)
 
     override fun setUpViews() {
+        val userNameDeepLink = args.username
+        //if username is null since edit text supports null value it will just remain empty
+
+        binding.editTextUsername.setText(userNameDeepLink)
         binding.buttonConfirm.setOnClickListener {
             val action =
                 LoginFragmentDirections.actionLoginFragmentToWelcomeFragment(binding.editTextUsername.text.toString())
